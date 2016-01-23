be61e73c72d1a58234fa3eb3c930893fe3876355
Ashish Malhotra <ash6320@gmail.com>
2016-01-22 19:07:43 -0500

    Add insert ability

--- testConn.py
+++ testConn.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python
 
 import pymssql
+import datetime
+
+now = datetime.datetime.now()
+
 cnxn = pymssql.connect("plexdb.database.windows.net","plex-admin@plexdb","OxygnClub123","plexdb")
 cursor = cnxn.cursor()
-cursor.execute("""
-select * from information_schema.tables
-""")
 
-rows = cursor.fetchall()
-for row in rows:
-	print row
+queryStr = "INSERT INTO user_profile (userId, blockStartTime, hardAccelerationCount, hardBrakingCount) OUTPUT INSERTED.userId VALUES ('%s','%s', %s, %s)" % ('jovan', now.strftime('%Y-%m-%d %H:%M:%S'), 1, 2)
+# print queryStr
+
+cursor.execute(queryStr)
+# rows = cursor.fetchall()
+# for row in rows:
+# 	print str(row[0])
+
+cnxn.commit()
