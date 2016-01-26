fb307e3f5c20d89da7294b9f854027fa085ed158
Jovan Sardinha <jovan.sardinha@gmail.com>
2016-01-26 12:56:27 -0500

    cleaned code for files

--- model_building.py
+++ model_building.py
@@ -1,3 +1,7 @@
+# PURPOSE: This scripts is used for model building and traning.
+# GOAL: To produce a model and output it in the ./model folder.
+
+# Libraries used
 from pymongo import MongoClient
 import pandas as pd
 import datetime
@@ -14,15 +18,17 @@ import matplotlib.dates as md
 from sklearn.learning_curve import learning_curve
 from sklearn.externals import joblib
 
+# Used to time the total script run time.
+# Notes: Average (historic) run times of the script = ~2 seconds
 time_start = time.time()
 ###############################
 
-
-# Working example with pymongo
-#mongo_uri = 'mongodb://%s:%s@%s:%s/%s' % (username, password, host, port, db)
+# Step 1: Reading data from mongo
+#mongo_uri = 'mongodb://%s:%s@%s:%s/%s' % (username, password, host, port, db) # For future refrence incase we add username and password to DB
 client = MongoClient('40.122.215.160')
 db = client['archive[2016-01-13@13:08ET]_test']
 
+# Reading data from collections
 collection_acceleration = db['acceleration']
 #collection_gyroscope = db['gyroscope']
 #collection_linearAcceleration = db['linearAcceleration']
@@ -30,7 +36,7 @@ collection_acceleration = db['acceleration']
 #collection_orientation = db['orientation']
 #collection_rotation = db['rotation']
 
-
+# Importing collection data into pandas data frame for easy manipulation
 df_acceleration = pd.DataFrame(list(collection_acceleration.find()))
 #df_gyroscope = pd.DataFrame(list(collection_gyroscope.find()))
 #df_linearAcceleration = pd.DataFrame(list(collection_linearAcceleration.find()))
@@ -38,7 +44,7 @@ df_acceleration = pd.DataFrame(list(collection_acceleration.find()))
 #df_orientation = pd.DataFrame(list(collection_orientation.find()))
 #df_rotation = pd.DataFrame(list(collection_rotation.find()))
 
-
+# Step 2: Data pre-processing
 time_converter = lambda x: datetime.datetime.fromtimestamp(float(x)/1000)
 
 df_acceleration['timestamp'] = df_acceleration['timestamp'].map(time_converter)
@@ -54,24 +60,24 @@ def acceleration_magnitude(x,y,z):
 df_acceleration['acceleration_magnitude'] = np.vectorize(acceleration_magnitude)(df_acceleration['x'],df_acceleration['y'],df_acceleration['z'])
 
 
-# Data Splitting
+# Step 3: Data Splitting
 # Notes: Using the data split as advised by Andrew Ng. ->  20% Test, 20% CV 60% Train
 df_total = df_acceleration[['isDriving', 'x', 'y', 'z', 'acceleration_magnitude']].copy()
 df_total['isDriving'] = df_total['isDriving'].map({'true': 1, 'false': 0}).astype(int)
 data_total = df_total.values
 data_train, data_test = cross_validation.train_test_split(data_total, test_size=0.20, random_state=13)
 
-# Model Building
+# Step 4: Model Building
 model = SVC()
 model = model.fit(data_train[:,1:], data_train[:,0])
 
-# Predicting
+# Step 5: Predicting
 output = model.predict(data_test[:,1:])
 
-# Saving the model
+# Step 6: Saving the model
 joblib.dump(model, './model/plex_model.pkl')
 
-
+# Step 7: Post-Analysis
 # Parameters:
 # Base Parameters
 accuracy = skm.accuracy_score(data_test[:,0], output)
@@ -87,7 +93,7 @@ roc_auc = skm.roc_auc_score(data_test[:,0], output)
 log_loss = skm.log_loss(data_test[:,0], output)
 
 
-'''
+
 # Training Curves
 
 def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
@@ -153,35 +159,9 @@ cv = cross_validation.ShuffleSplit(data_train.shape[0], n_iter=6,test_size=0.2, 
 train_sizes, train_scores, valid_scores = learning_curve(estimator=model, X=data_train[:,1:], y=data_train[:,0], cv=cv)
 
 plot_learning_curve(model, "Plex.ai Test", X=data_train[:,1:], y=data_train[:,0], cv=cv )
-'''
 
 ##################################
+# End of script: The following is used to calculate total run times
 time_end = time.time()
 time_total = time_end - time_start
 print("--- Run time is: %s seconds ---" % (time_total))
-
-
-
-
-#Graveyard
-'''
-# Plotting
-import matplotlib.pyplot as plt
-import matplotlib.dates as md
-
-#ax=plt.gca()
-#xfmt = md.DateFormatter('%H:%M:%S')
-#ax.xaxis.set_major_formatter(xfmt)
-plt.plot(fft_tdf, 'g^')
-plt.show()
-
-
-signal = np.array([-2, 8, 6, 4, 1, 0, 3, 5], dtype=float)
-count = np.array([1,2,3,4,5,6,7,8])
-df = pd.DataFrame(signal, count)
-df['temp'] =
-fourier = np.fft.fft(signal)
-n = signal.size
-timestep = 0.1
-freq = np.fft.fftfreq(n, d=timestep)
-'''
